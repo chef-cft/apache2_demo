e2f9b6c86b1b7dcfb2f6eea3c69cfda337712a65
Gavin Reynolds <gavin@chef.io>
2018-09-25 12:55:56 +0200

    Add linux-baseline profile
    Signed-off-by: Gavin Reynolds <gavin@chef.io>

--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -5,14 +5,30 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
 
-unless os.windows?
-  # This is an example test, replace with your own test.
-  describe user('root'), :skip do
-    it { should exist }
-  end
+if os.family == 'debian'
+  apache_service = 'apache2'
+  apache_user = 'www-data'
+elsif os.family == 'redhat'
+  apache_service = 'httpd'
+  apache_user = 'apache'
 end
 
-# This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+describe port(80) do
+  it { should be_listening }
+end
+
+describe service(apache_service) do
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe file('/var/www/html/basic_web_app/index.html') do
+  it { should be_owned_by 'root' }
+  it { should be_readable.by_user(apache_user) }
+  its('mode') { should cmp '0644' }
+end
+
+describe http('http://127.0.0.1/') do
+  its('status') { should eq 200 }
+  its('body') { should match 'Hello World' }
 end
